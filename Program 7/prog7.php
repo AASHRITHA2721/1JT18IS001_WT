fde7d1b37c7f0fb0fac57016b9805aad89faad29
AASHRITHA2721 <63099802+AASHRITHA2721@users.noreply.github.com>
2021-06-29 16:31:55 +0530

    Add files via upload

--- Program 7/prog7.php
+++ Program 7/prog7.php
@@ -1,1 +1,33 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="refresh" content="1" />
+<style>
+p {
+color: black;
+font-size: 40px;
+font-family: "Brush Script MT","Lucida Handwriting", Cursive;
+position: absolute;
+top: 50%;
+left: 50%;
+transform: translate(-50%, -50%);
+}
+body {
+background-color:royalblue;
+}
+</style>
+<body>
+    <p>
+        <?php
+ echo "The server time is ". date("h:i:s A") . "<br>" ;
+ echo "Today is " . date("Y/m/d") ."<br>";
+echo "Today is " . date("Y.m.d") ."<br>";
+echo "Today is " . date("Y-m-d") ."<br>";
+echo "Today is " . date("l");
+?>
+</p>
 
+
+</body>
+</head>
+</html>
