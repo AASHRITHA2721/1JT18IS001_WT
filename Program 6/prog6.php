43e3a56d052a79ddc15a39a6e217700ebb672412
AASHRITHA2721 <63099802+AASHRITHA2721@users.noreply.github.com>
2021-06-29 16:30:43 +0530

    Add files via upload

--- Program 6/prog6.php
+++ Program 6/prog6.php
@@ -1,1 +1,32 @@
-
+<style>
+    h2{
+        text-align:center;
+        font-family:"Brush Script MT","Lucida Handwriting", Cursive;
+    }
+    body{
+        background-color:lightpink;
+    }
+    </style>
+<?php
+$user = "Aashritha";
+?>
+<!DOCTYPE html>
+<html>
+<head> <title> program 6 </title></head>
+<body>
+<h2>Hey <?php echo $user;?>, Welcome to this page! </h2>
+<h3>This page displays the number of page visits</h3>
+<?php
+print "<h3> REFRESH PAGE </h3>";
+$name="counter.txt";
+$file = fopen($name,"r");
+$hits = fscanf($file,"%d");
+fclose($file);
+$hits[0]++;
+$file = fopen($name,"w");
+fprintf($file,"%d",$hits[0]);
+fclose($file);
+echo "<b> Total number of views: </b>".$hits[0];
+?>
+</body>
+</html>
